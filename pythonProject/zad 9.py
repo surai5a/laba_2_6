b4954dcba84d4dd6a358f0c43d31d0824cf63473
Vadim <vadym.prymary@gmail.com>
2021-12-11 16:55:02 +0300

    Create zad 9.py

---
+++ pythonProject/zad 9.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+school = {'1a': '19', '3б': '15', '4в': '23', '5a': '18'}
+sum = 0
+
+for i in school:
+    sum += int(school[i])
+
+print(f"Общее количество учащихся: {sum}")
